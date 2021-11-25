392f55c381de04306962c41d08864c70fd0f244f
Keylane <keylanessilva07@gmail.com>
2021-11-25 10:06:11 -0300

    add card

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import PrimeiroComponente from './components/basicos/PrimeiroComp'
 import ComParametro from './components/basicos/ComParametro'
-
 import Aleatorio from './components/aleatorio/Aleatorio'
+import Card from './components/layout/Card'
 
 // usando arrow function av
 export default _ => 
-        <div id="app">
-        {/* <PrimeiroComponente />
-        <ComParametro s
-            aluno = 'Ricardo'
-            nota = '8.7'/> */}
+        <div>
+        <PrimeiroComponente 
+            msg='Fundamentos React'
+        />
+
+        <Card 
+            titulo="Desafio #01">
             <Aleatorio min='1' max='10'/>
+        </Card>
+        <Card 
+            titulo="Desafio #02">
+            <ComParametro s
+            aluno = 'Ricardo'
+            nota = '8.7'/>
+        </Card>
+        
         </div>
     
