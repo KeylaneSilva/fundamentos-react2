e6e03ce93f8dbbe7f0f00372b782451aecd6b97b
Keylane <keylanessilva07@gmail.com>
2021-11-26 09:59:11 -0300

    add estilo por prps

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,39 @@ import PrimeiroComponente from './components/basicos/PrimeiroComp'
 import ComParametro from './components/basicos/ComParametro'
 import Aleatorio from './components/aleatorio/Aleatorio'
 import Card from './components/layout/Card'
+import './App.css'
 
 // usando arrow function av
 export default _ => 
-        <div>
-        <PrimeiroComponente 
-            msg='Fundamentos React'
-        />
+        <div className="App">
+                <PrimeiroComponente 
+                    msg='Fundamentos React'
+                />
+            <div className="CardConf">
 
-        <Card 
-            titulo="Desafio #01">
-            <Aleatorio min='1' max='10'/>
-        </Card>
-        <Card 
-            titulo="Desafio #02">
-            <ComParametro s
-            aluno = 'Ricardo'
-            nota = '8.7'/>
-        </Card>
-        
+                <Card 
+                    // passando a cor dinamicamente por props
+                    titulo="Desafio #01" color="#080">
+                    <Aleatorio min='1' max='10'/>
+                </Card>
+                <Card 
+                    titulo="Desafio #02" color="#48D1CC">
+                    <ComParametro s
+                    aluno = 'Ricardo'
+                    nota = '8.7'/>
+                </Card>
+                <Card 
+                    titulo="Desafio #03" color="#EE82EE">
+                    <ComParametro s
+                    aluno = 'Ricardo'
+                    nota = '8.7'/>
+                </Card>
+                <Card 
+                    titulo="Desafio #04" color="#FFD700">
+                    <ComParametro s
+                    aluno = 'Ricardo'
+                    nota = '8.7'/>
+                </Card>
+            </div>
         </div>
     
--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -3,8 +3,15 @@ import './Card.css'
 
 
 export default function Card(props) {
+    
+    // recebendo estilo por props
+    const styleCard = {
+        backgroundColor : props.color || '#ff0000',
+        borderColor: props.color || '#ff0000'
+    }
+    
     return (
-        <div className="Card">
+        <div className="Card" style={styleCard}>
             <div className="Title">{props.titulo}</div>
             <div className="Content">{props.children}</div>
         </div>
