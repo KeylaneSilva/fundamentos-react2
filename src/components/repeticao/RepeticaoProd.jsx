3a38e802a31212db5ab78763269030f095efe61c
Keylane <keylanessilva07@gmail.com>
2021-12-01 09:06:33 -0300

    refatorando tabela

--- src/components/repeticao/RepeticaoProd.jsx
+++ src/components/repeticao/RepeticaoProd.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import produtos from '../../data/listaProdutos'
 import '../repeticao/RepeticaoProd.css'
 
 export default function RepeticaoProd() {
-
-    const tabelaProd = produtos.map((produto) => 
+    const [valor, setValor] = useState(0)
+    
+    const tabelaProd = produtos.map((produto) =>         
         <tr>
             <td>{produto.id}</td>
             <td>{produto.produto}</td>
             <td>{produto.valor}</td>
         </tr>
-    )
+    ) 
+
+    function total(){
+        let acm = 0
+        for(let i = 0; i< produtos.length; i++){
+            acm += produtos[i].valor
+        }
+        document.getElementById('total').innerHTML = acm.toFixed(2)
+    }
+
 
     return (
         <div>
@@ -25,7 +35,15 @@ export default function RepeticaoProd() {
                 <tbody>
                     {tabelaProd}
                 </tbody>
+                <tfoot>
+                    <tr>
+                        <td>Total</td>
+                        <td>-</td>
+                        <td id="total"></td>
+                    </tr>
+                </tfoot>
             </table>
+                    <button onClick={total}>Calcular total</button>
         </div>
     )
 }
